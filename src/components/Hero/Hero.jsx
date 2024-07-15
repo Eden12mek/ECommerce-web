95f817d45307b799dd41f992e78c434a0041e447
Eden <eden12mekonnen@gmail.com>
2024-07-15 11:18:46 +0300

    modification

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,14 +8,14 @@ const ImageList = [
     {
    id: 1,
     img: Image1,
-    title: "Upto 50% off on all Men's Wear",
+    title: "Upto 50% off on all Women's Wear",
     description:
       "lorem His Life will forever be Changed dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
   },
   {
     id: 2,
     img: Image2,
-    title: "30% off on all Women's Wear",
+    title: "30% off on all Men's Wear",
     description:
       "Who's there lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
   },
